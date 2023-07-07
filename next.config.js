b22f07462694d6bdd0881ebc9f4ea1eb3c3aee80
zerospeedzero <zerospeedzero@gmail.com>
2023-07-06 22:55:41 -0600

    Change next.confg.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'gateway.ipfscdn.io'],
+    unoptimized: true,
   }
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Navbar from '../Components/Navbar'
 import { ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 import { Goerli } from "@thirdweb-dev/chains";
 import '@/styles/globals.css'
-import { ThemeProvider } from 'next-themes';
+// import { ThemeProvider } from 'next-themes';
 import Header from '@/Components/Header';
 
 export default function App({ Component, pageProps }) {
@@ -13,10 +13,10 @@ export default function App({ Component, pageProps }) {
       rpc: ["https://goerli.infura.io/v3/19a6a39387da412c885d134c648d857b"]
     }}
   > 
-    <ThemeProvider>
+    {/*<ThemeProvider>*/}
       <Header/>
       <Component {...pageProps} />
-    </ThemeProvider>
+    {/*</ThemeProvider> */}
   </ThirdwebProvider>
   )
 }
