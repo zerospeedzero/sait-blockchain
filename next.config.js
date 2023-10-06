fe59a0e662fdb6cae4491911a4942296e0a3bd77
zerospeedzero <36888299+zerospeedzero@users.noreply.github.com>
2023-10-06 00:18:57 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'gateway.ipfscdn.io'],
     unoptimized: true,
